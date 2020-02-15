ccbebd5fdf7f3c4a211643ebc5f283f9ef96193a
santi <s.britos@hotmail.com>
2020-02-16 00:21:31 +0100

    se modifico el solicitud productos blabla

--- app/src/main/java/com/santi/imagine/ui/SolicitudProductos.java
+++ app/src/main/java/com/santi/imagine/ui/SolicitudProductos.java
@@ -1,16 +1,17 @@
 package com.santi.imagine.ui;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -59,95 +60,111 @@ public class SolicitudProductos extends AppCompatActivity {
         tv5 = (TextView)findViewById(R.id.tv5);
         pb = (ProgressBar) findViewById(R.id.pb);
 
-        progressbar(false);
-
-        FirebaseUser datos_usuario = firebaseAuth.getCurrentUser();
-
-
-        DocumentReference useragra = db.collection("Usuarios").document(datos_usuario.getUid());
-
-        useragra.get().addOnSuccessListener(    new OnSuccessListener<DocumentSnapshot>() {
+        AlertDialog.Builder cerrar = new AlertDialog.Builder(SolicitudProductos.this);
+        cerrar.setTitle("Solicitud");
+        cerrar.setMessage("¿Estas seguro de solicitar este producto?").setCancelable(false).setPositiveButton("Si", new DialogInterface.OnClickListener() {
             @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                usuarioagra = documentSnapshot.toObject(Usuarios.class);
+            public void onClick(DialogInterface dialogInterface, int i) {
 
-            }
-        });
+                FirebaseUser datos_usuario = firebaseAuth.getCurrentUser();
 
 
-        DocumentReference docref = db.collection("Usuarios").document(tokenUsuario1);
+                DocumentReference useragra = db.collection("Usuarios").document(datos_usuario.getUid());
 
-        docref.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                usuario = documentSnapshot.toObject(Usuarios.class);
-                tv2.setText("Gracias " +  usuarioagra.getNombreCompleto());
-                tv3.setText("Le has solicitado a " + usuario.getNombreCompleto());
-                tv4.setText(producto);
-                tv5.setText(cantidad + " unidades");
-                Picasso.get().load(imagen).into(im);
+                useragra.get().addOnSuccessListener(    new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        usuarioagra = documentSnapshot.toObject(Usuarios.class);
+
+                    }
+                });
 
 
-                final Intent email = new Intent(Intent.ACTION_SEND);
-                email.setData(Uri.parse("mailto:"));
+                DocumentReference docref = db.collection("Usuarios").document(tokenUsuario1);
 
-                email.putExtra(Intent.EXTRA_EMAIL,new String[]{usuario.getEmail()});
-                email.putExtra(Intent.EXTRA_TEXT,"Contacte a su donador");
-                email.setType("plain/text");
+                docref.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        usuario = documentSnapshot.toObject(Usuarios.class);
+                        tv2.setText("Gracias " +  usuarioagra.getNombreCompleto());
+                        tv3.setText("Le has solicitado a " + usuario.getNombreCompleto());
+                        tv4.setText(producto);
+                        tv5.setText(cantidad + " unidades");
+                        Picasso.get().load(imagen).into(im);
 
+
+                        final Intent email = new Intent(Intent.ACTION_SEND);
+                        email.setData(Uri.parse("mailto:"));
+
+                        email.putExtra(Intent.EXTRA_EMAIL,new String[]{usuario.getEmail()});
+                        email.putExtra(Intent.EXTRA_TEXT,"Contacte a su donador");
+                        email.setType("plain/text");
+
+
+                        Thread timer = new Thread(){
+                            public void run() {
+                                try {
+                                    sleep(2500);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                } finally {
+                                    startActivity(Intent.createChooser(email,"Elige un cliente de email"));
+                                    finish();
+
+                                }
+                            }
+                        };
+                        timer.start();
+
+
+
+                    }
+                });
+
+
+                db.collection("Productos").document(producto+"_"+tokenUsuario1)
+                        .delete()
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+
+
+                                Log.i("PRODUCTO BORRADO CON EXITO","se ha borrado el producto " + producto);
+                            }
+                        });
 
                 Thread timer = new Thread(){
                     public void run() {
                         try {
-                            sleep(2500);
+                            sleep(2600);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         } finally {
-                            startActivity(Intent.createChooser(email,"Elige un cliente de email"));
-                            finish();
+                            startActivity(new Intent(SolicitudProductos.this,Principal.class));
+
 
                         }
                     }
                 };
                 timer.start();
 
-
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+                dialogInterface.cancel();
 
             }
         });
-
-
+        cerrar.show();
 
             }
 
     @Override
     protected void onStart() {
         super.onStart();
-        db.collection("Productos").document(producto+"_"+tokenUsuario1)
-                .delete()
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
 
-
-                        Log.i("PRODUCTO BORRADO CON EXITO","se ha borrado el producto " + producto);
-                    }
-                });
-
-        Thread timer = new Thread(){
-            public void run() {
-                try {
-                    sleep(2600);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    startActivity(new Intent(SolicitudProductos.this,Principal.class));
-
-
-                }
-            }
-        };
-        timer.start();
     }
 
 
